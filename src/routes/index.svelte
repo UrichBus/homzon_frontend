0f6dafa2f367a1cd0ad6e7efd71466907c220ea0
UrichBus <urichbus@gmail.com>
2022-09-25 12:46:12 +0000

    product page changes
    products is now exported from index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,66 +1,99 @@
-<script context='module'>
-  export const prerender = true
+<script context="module">
+  export const prerender = true;
 
-  let products, location, fetchIt = 0
+  let products,
+    location,
+    fetchIt = 0;
 
   //fetch all products from strapi db
   export async function load({ fetch }) {
-    if(fetchIt === 0) {
-      await fetch(`${import.meta.env.VITE_HOST_URL}/api/products?populate=image&fields=name,price,miniDescription`)
-      .then(res => res.json())
-      .then(data => products = data.data)
-      .catch(err => console.log(err))
+    if (fetchIt === 0) {
+      await fetch(
+        `${
+          import.meta.env.VITE_HOST_URL
+        }/api/products?populate=image&fields=name,price,miniDescription,description`
+      )
+        .then((res) => res.json())
+        .then((data) => (products = data.data))
+        .catch((err) => console.log(err));
 
-      await fetch(`https://ipinfo.io/json?token=${import.meta.env.VITE_IP_TOKEN}`)
-      .then(res => res.json())
-      .then(data => location = {
-        city: data.city,
-        country: data.country == 'GH' ? 'Ghana' : '',
-        region: data.region,
-        countryCode: data.country
-      })
-      .catch(err => {})
+      await fetch(
+        `https://ipinfo.io/json?token=${import.meta.env.VITE_IP_TOKEN}`
+      )
+        .then((res) => res.json())
+        .then(
+          (data) =>
+            (location = {
+              city: data.city,
+              country: data.country == "GH" ? "Ghana" : "",
+              region: data.region,
+              countryCode: data.country,
+            })
+        )
+        .catch((err) => {});
     }
-    fetchIt++
+    fetchIt++;
   }
+
+  export { products };
 </script>
 
 <script>
-  import { browser } from '$app/env'
-  import { getAnalytics } from 'firebase/analytics'
-  
-  import MainDisplay from '../components/MainDisplay.svelte'
-  
-  import app from '../stores/fb'
-  import { ghanaMult } from '../stores/pay'
-  import { isGhana } from '../stores/stores'
+  import { browser } from "$app/env";
+  import { getAnalytics } from "firebase/analytics";
+
+  import MainDisplay from "../components/MainDisplay.svelte";
+
+  import app from "../stores/fb";
+  import { ghanaMult } from "../stores/pay";
+  import { isGhana } from "../stores/stores";
 
   //check if location is defined then set isGhana to it's value
-  location && ($isGhana = location)
+  location && ($isGhana = location);
 
   //initiate firebase analytics
-  browser && getAnalytics(app)
+  browser && getAnalytics(app);
 </script>
 
 <MainDisplay />
-<h1 id='products_display' class='my-5 text-center text-lg font-bold'>Products</h1>
-<div class='flex flex-wrap justify-center'>
-  {#each products as product ,i}
-    <div class='w-full p-4 max-w-sm'>
-        <a sveltekit:prefetch href={'product/'+product.id} class='c-card block bg-white shadow-md hover:shadow-xl rounded-sm overflow-hidden'>
-        <div class='relative pb-2 overflow-hidden'>
-          <img class='relative inset-0 h-60 w-full object-contain' src={`${product.attributes.image.data[0].attributes.url}`} alt={product.name}>
+<h1 id="products_display" class="my-5 text-center text-lg font-bold">
+  Products
+</h1>
+<div class="flex flex-wrap justify-center">
+  {#each products as product, i}
+    <div class="w-full p-4 max-w-sm">
+      <a
+        sveltekit:prefetch
+        href={"product/" + product.id}
+        class="c-card block bg-white shadow-md hover:shadow-xl rounded-sm overflow-hidden"
+      >
+        <div class="relative pb-2 overflow-hidden">
+          <img
+            class="relative inset-0 h-60 w-full object-contain"
+            src={`${product.attributes.image.data[0].attributes.url}`}
+            alt={product.name}
+          />
         </div>
-        <div class='p-4'>
-          <h2 class='mt-2 mb-2  font-bold'>{product.attributes.name}</h2>
-          <p class='text-sm'>{product.attributes.miniDescription.substring(0, 40)}...</p>
-          <div class='mt-3 flex items-center'>
-            <span class='text-sm font-semibold'>{location && location.countryCode == 'GH' ? 'gh' : 'usd'}</span>&nbsp;
-            <span class='font-bold text-xl'>{location && location.countryCode == 'GH' ? (product.attributes.price * ghanaMult).toFixed(2) : product.attributes.price}</span>&nbsp;
-            <span class='text-sm font-semibold'>{location && location.countryCode == 'GH' ? 'C' : '€'}</span>
+        <div class="p-4">
+          <h2 class="mt-2 mb-2  font-bold">{product.attributes.name}</h2>
+          <p class="text-sm">
+            {product.attributes.miniDescription.substring(0, 40)}...
+          </p>
+          <div class="mt-3 flex items-center">
+            <span class="text-sm font-semibold"
+              >{location && location.countryCode == "GH" ? "gh" : "usd"}</span
+            >&nbsp;
+            <span class="font-bold text-xl"
+              >{location && location.countryCode == "GH"
+                ? (product.attributes.price * ghanaMult).toFixed(2)
+                : product.attributes.price}</span
+            >&nbsp;
+            <span class="text-sm font-semibold"
+              >{location && location.countryCode == "GH" ? "C" : "€"}</span
+            >
           </div>
         </div>
-        </a>
+      </a>
     </div>
   {/each}
 </div>
--- src/routes/product/[id].svelte
+++ src/routes/product/[id].svelte
@@ -1,94 +1,96 @@
 <script>
-    import { page } from '$app/stores'
-    import { browser } from '$app/env'
-    import { onMount } from 'svelte/internal'
-    import { goto } from '$app/navigation'
-    import SvelteMarkdown from 'svelte-markdown'
-    
-    import { ghanaMult } from '../../stores/pay'
-    import { products, total, subtotal, isGhana } from '../../stores/stores'
+  import { page } from "$app/stores";
+  import { browser } from "$app/env";
+  import { goto } from "$app/navigation";
+  import SvelteMarkdown from "svelte-markdown";
 
-    export const prerender = true
+  import { ghanaMult } from "../../stores/pay";
+  import { products, total, subtotal, isGhana } from "../../stores/stores";
+  import { products as displayProduct } from "../index.svelte";
 
-    //the first variable retrieves the id from the route
-    let id = $page.params.id, product, readmore = false
+  export const prerender = true;
 
-    if(id == 'home') goto('/')
+  //the first variable retrieves the id from the route
+  let id = $page.params.id,
+    product,
+    readmore = false;
 
-    onMount(async() => {
-        //this fetchs the specific products
-        fetch(`${import.meta.env.VITE_HOST_URL}/api/products/${id}?populate=image&fields=name,price,miniDescription,description`)
-        .then(res => res.json())
-        .then(data => product = data.data)
-        .catch(err => console.log(err))  
-    })
+  if (id == "home") goto("/");
 
+  product = displayProduct.filter((el) => el.id === Number(id));
+  product = product[0];
 
-    function addProduct(id) {
-        const productExist = $products.find(element => element.id === id)
-        let thePrice 
+  function addProduct(id) {
+    const productExist = $products.find((element) => element.id === id);
+    let thePrice;
 
-        if(productExist) alert('Product already added to cart')
-        else {
-            thePrice = $isGhana.country || $isGhana.countryCode == 'GH' ? product.attributes.price * ghanaMult : product.attributes.price
-            $products.push({
-                id: product.id,
-                name: product.attributes.name,
-                price: thePrice,
-                image: `${product.attributes.image.data[0].attributes.url}`
-            })
-            browser && window.localStorage.setItem('products', JSON.stringify($products))
-            $products = $products
-            $subtotal = $subtotal + thePrice
-            $total = $total + thePrice
-        }
+    if (productExist) alert("Product already added to cart");
+    else {
+      thePrice =
+        $isGhana.country || $isGhana.countryCode == "GH"
+          ? product.attributes.price * ghanaMult
+          : product.attributes.price;
+      $products.push({
+        id: product.id,
+        name: product.attributes.name,
+        price: thePrice,
+        image: `${product.attributes.image.data[0].attributes.url}`,
+      });
+      browser &&
+        window.localStorage.setItem("products", JSON.stringify($products));
+      $products = $products;
+      $subtotal = $subtotal + thePrice;
+      $total = $total + thePrice;
     }
+  }
 
-    const handleRead = () => readmore ? readmore = false : readmore = true
+  const handleRead = () => (readmore ? (readmore = false) : (readmore = true));
 </script>
 
-{#if !product}
-<div class='animate-pulse grid gap-4 w-screen p-2 md:flex md:p-4'>
-    <div class='md:flex-1 mx-auto'>
-        <img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921' alt='animation' class='min-w-sm md:max-w-md lg:max-w-lg'>
+<div class="grid gap-4 w-screen p-2 md:flex md:p-4">
+  <div class="md:flex-1 mx-auto lg:justify-items-center items-center">
+    <img
+      src={`${product.attributes.image.data[0].attributes.url}`}
+      alt={product.name}
+      class="min-w-sm md:max-w-md lg:max-w-lg lg:ml-4"
+    />
+  </div>
+  <div class="md:flex-1 p-4">
+    <h1 class="my-2 text-base">
+      <strong>Product name:</strong>
+      {product.attributes.name}
+    </h1>
+    <p class="mb-2">
+      <strong
+        >Price: {$isGhana.country || $isGhana.countryCode == "GH"
+          ? "GHC"
+          : "usd"}
+        {$isGhana.country || $isGhana.countryCode == "GH"
+          ? (product.attributes.price * ghanaMult).toFixed(2)
+          : product.attributes.price}</strong
+      >
+    </p>
+    <h2 class="font-semibold">Description</h2>
+    <h3 class="mb-4">{product.attributes.miniDescription}</h3>
+    {#if readmore}
+      <SvelteMarkdown source={product.attributes.description} />
+      <button
+        class="p-2 border border-slate-200 rounded-sm"
+        on:click={handleRead}>Readless</button
+      >
+    {:else}
+      <button
+        class="p-2 border border-slate-200 rounded-sm"
+        on:click={handleRead}>Readmore</button
+      >
+    {/if}
+    <div>
+      <button
+        on:click={() => addProduct(product.id)}
+        class="bg-slate-800 mt-4 border border-transparent rounded-sm shadow-sm py-2 px-8 text-sm font-medium text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-400"
+      >
+        Add To Cart
+      </button>
     </div>
-    <div class='md:w-1/2 md:flex-1 p-6'>
-        <div class='md:w-1/2 my-2 h-2 bg-slate-200 rounded' />
-        <div class='md: w-1/2 mb-4 h-2 bg-slate-200 rounded' />
-        <div class='md:w-1/2 space-y-3'>
-            <div class='grid grid-cols-3 gap-4'>
-              <div class='h-2 bg-slate-200 rounded col-span-2'></div>
-              <div class='h-2 bg-slate-200 rounded col-span-1'></div>
-            </div>
-            <div class='h-2 bg-slate-200 rounded'></div>
-        </div>
-    </div>
+  </div>
 </div>
-{/if}
-
-{#if product}
-    <div class='grid gap-4 w-screen p-2 md:flex md:p-4'>
-        <div class='md:flex-1 mx-auto lg:justify-items-center items-center'>
-            <img src={`${product.attributes.image.data[0].attributes.url}`} alt={product.name} class='min-w-sm md:max-w-md lg:max-w-lg lg:ml-4'>
-        </div>
-        <div class='md:flex-1 p-4'>
-            <h1 class='my-2 text-base'><strong>Product name:</strong> {product.attributes.name}</h1>
-            <p class='mb-2'>
-                <strong>Price: {$isGhana.country || $isGhana.countryCode == 'GH' ? 'GHC' : 'usd'} {$isGhana.country || $isGhana.countryCode == 'GH' ? (product.attributes.price * ghanaMult).toFixed(2) : product.attributes.price}</strong>
-            </p>
-            <h2 class='font-semibold'>Description</h2>
-            <h3 class='mb-4'>{product.attributes.miniDescription}</h3>
-            {#if readmore}
-                <SvelteMarkdown source = {product.attributes.description} />
-                <button class='p-2 border border-slate-200 rounded-sm' on:click={handleRead}>Readless</button>
-            {:else}
-                <button class='p-2 border border-slate-200 rounded-sm' on:click={handleRead}>Readmore</button>
-            {/if}
-            <div>
-                <button on:click={() => addProduct(product.id)} class='bg-slate-800 mt-4 border border-transparent rounded-sm shadow-sm py-2 px-8 text-sm font-medium text-white hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-400'>
-                    Add To Cart
-                </button>
-            </div>
-        </div>
-    </div>
-{/if}
